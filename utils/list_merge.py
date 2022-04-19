2e82a275c92a4732ed506dba63cf69a49f52483f
666greatChina888 <666greatChina888@gmail.com>
2022-04-19 23:52:04 +0800

    update list_merge.py

--- utils/list_merge.py
+++ utils/list_merge.py
@@ -75,7 +75,7 @@ class sub_merge:
             content_raw,
             'content',
             'YAML',
-            {'dup_rm_enabled': True, 'format_name_enabled': True, 'speedtest': True},
+            {'dup_rm_enabled': False, 'format_name_enabled': True, 'speedtest': False},
         )
         content = content_raw
 
